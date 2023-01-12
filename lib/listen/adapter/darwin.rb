b05590c6c7150bb5dc9c433bc040e1004400249d
Nirankar Singh <nirankarsingh247@gmail.com>
2023-01-12 17:38:36 +0530

    Update darwin.rb

--- lib/listen/adapter/darwin.rb
+++ lib/listen/adapter/darwin.rb
@@ -64,11 +64,6 @@ module Listen
       def _process_event(dir, path)
         Listen.logger.debug { "fsevent: processing path: #{path.inspect}" }
         # TODO: does this preserve symlinks?
-        path  = "/AZ_collab/inbox/" + "base\255"
-        if invalid_encoded_file?(path)
-          _queue_change(:file, dir, path, invalid_file_path: true)
-          return
-        end
         rel_path = path.relative_path_from(dir).to_s
         _queue_change(:dir, dir, rel_path, recursive: true)
       end
